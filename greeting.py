28ec8c2c7b3f31f8d0d89e1406916d3b9857746c
mystickoder <89115482+mystickoder@users.noreply.github.com>
2023-01-31 23:03:14 -0500

    My Greetings

---
+++ greeting.py
@@ -0,0 +1,8 @@
+print("Welcome to Tobys")
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+age = input ("What is our age? ")
+
+message = (f"Greetings, {first_name} {last_name} it nice to meet you. Wow you are {age} years old. You look great!")
+print(message)
+
